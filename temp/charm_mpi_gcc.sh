2cc2807e847c7a7418125fe811bb14645a3f21f8
Chang-chia-hsiang <65059935+Chang-chia-hsiang@users.noreply.github.com>
2020-06-16 22:07:33 +0800

    Update charm_mpi_gcc.sh

--- temp/charm_mpi_gcc.sh
+++ temp/charm_mpi_gcc.sh
@@ -1,4 +1,4 @@
-WORK_DIR= /root
+WORK_DIR=/root
 CODE_NAME=charm
 GIT_DIR=${WORK_DIR}/${CODE_NAME}.git
 git clone --bare https://github.com/UIUC-PPL/charm.git ${GIT_DIR}
@@ -9,13 +9,13 @@ WORK_TREE=${WORK_DIR}
 CODE_DIR=${WORK_TREE}/${CODE_NAME}-${GIT_TAG}
 mkdir -p ${WORK_TREE} ${CODE_DIR}
 git --bare --git-dir=${GIT_DIR} fetch --all --prune
-git --bare --git-dir=${GIT_DIR} --work-tree=${WORK_TREE} reset --mixed ${GIT_TAG} --
+git --bare --git-dir=${GIT_DIR} --work-tree=${WORK_TREE} reset --mixed ${GIT_TAG}
 git --bare --git-dir=${GIT_DIR} --work-tree=${CODE_DIR} clean -fxdn
 git --bare --git-dir=${GIT_DIR} --work-tree=${WORK_TREE} checkout-index --force --all --prefix=${CODE_DIR}/
 
 # Build Charm++ v6.10.1
 CHARM_DIR=${CODE_DIR} \
-HPCX_DIR=${WORK_DIR}/hpcx-v2.6.0-gcc-MLNX_OFED_LINUX-4.7-1.0.0.1-ubuntu16.04-x86_64 \
+HPCX_DIR=${WORK_DIR}/sources/hpcx-v2.6.0-gcc-MLNX_OFED_LINUX-4.7-1.0.0.1-ubuntu16.04-x86_64 \
 HPCX_MPI_DIR=$HPCX_DIR/ompi \
 GCC=/usr/local/gcc-8.4.0/bin/gcc \
 NATIVE_GCC_FLAGS="-march=native -mtune=native -mavx2 -msse4.2 -O3 -DNDEBUG" \
@@ -28,7 +28,7 @@ CMD_BUILD_MPI_CHARM_GCC=" \
         && hpcx_load \
         && mkdir -p ${CHARM_DIR}/built \
         && cd ${CHARM_DIR}/built \
-        && time -p charm++ mpi-linux-x86_64 \
+        && time -p ../build charm++ mpi-linux-x86_64 \
         -j --with-production \
         --basedir=$HPCX_MPI_DIR \
         gcc gfortran $GCC_FLAGS \
